e21f1d776720fa6b74e2fcb8de5a4415c7a87733
Kitae's Github <aqua0405@gmail.com>
2023-04-17 02:13:22 +0900

    Create Customer.java

---
+++ src/main/java/inhatc/jap/jpastudy/customer/entity/Customer.java
@@ -0,0 +1,41 @@
+package inhatc.jap.jpastudy.customer.entity;
+
+import inhatc.jap.jpastudy.utils.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@Builder
+public class Customer extends BaseEntity {
+
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 7, unique = true)
+    private String customerCode;    // 고객코드
+
+    @Column(nullable = false, length = 10)
+    private String customerName;    // 고객명
+
+    @Column(nullable = false, length = 1)
+    private String flag;              //성별구분
+
+    @Column(nullable = false, length = 8)
+    private String birthDay;        // 생일
+
+    @Column(nullable = true, length = 16)
+    private String phoneNumber;     // 전화번호
+
+    @Column(nullable = true, length = 30)
+    private String email;           // 이메일
+
+    private int totalPoint;         // 누적포인트
+
+}
