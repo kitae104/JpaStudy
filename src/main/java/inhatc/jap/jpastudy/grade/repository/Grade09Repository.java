301ed937f2b213defe8f08a9f640affd1d892c1b
Kitae's Github <aqua0405@gmail.com>
2023-04-25 01:06:12 +0900

    Create Grade09Repository.java

---
+++ src/main/java/inhatc/jap/jpastudy/grade/repository/Grade09Repository.java
@@ -0,0 +1,7 @@
+package inhatc.jap.jpastudy.grade.repository;
+
+import inhatc.jap.jpastudy.grade.entity.Grade09;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Grade09Repository extends JpaRepository<Grade09, String> {
+}
