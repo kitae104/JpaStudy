4a73af38b3be7b46c80ba73155049979f2a00deb
Kitae's Github <aqua0405@gmail.com>
2023-04-24 17:35:06 +0900

    Create Point2017.java

---
+++ src/main/java/inhatc/jap/jpastudy/point/entity/Point2017.java
@@ -0,0 +1,33 @@
+package inhatc.jap.jpastudy.point.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@Builder
+public class Point2017 {
+
+    @Id
+    @Column(name = "customer_code", length = 7)
+    private String customerCode;    // 고객코드
+
+    @Id
+    @Column(precision = 10, scale = 0, columnDefinition = "NUMBER (10)")
+    private double seqNo;
+
+    @Column(length = 50)
+    private String pointMemo;
+
+    @Column(precision = 10, scale = 0, columnDefinition = "NUMBER (10)")
+    private double point;
+
+    @Column(length=14)
+    private String regDate;
+}
