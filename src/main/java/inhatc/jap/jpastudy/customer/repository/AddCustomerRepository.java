874822b3b3c64e3dbb5e603346c4e86c24ffe175
Kitae's Github <aqua0405@gmail.com>
2023-04-25 01:05:24 +0900

    Create AddCustomerRepository.java

---
+++ src/main/java/inhatc/jap/jpastudy/customer/repository/AddCustomerRepository.java
@@ -0,0 +1,7 @@
+package inhatc.jap.jpastudy.customer.repository;
+
+import inhatc.jap.jpastudy.customer.entity.AddCustomer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddCustomerRepository extends JpaRepository<AddCustomer, String> {
+}
