92d59af2aee3276ea5a86b025160f2060aea7eb8
Kitae's Github <aqua0405@gmail.com>
2023-04-25 01:06:30 +0900

    Create EtcInfo.java

---
+++ src/main/java/inhatc/jap/jpastudy/info/entity/EtcInfo.java
@@ -0,0 +1,26 @@
+package inhatc.jap.jpastudy.info.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@Builder
+public class EtcInfo {
+
+    @Id
+    @Column(length = 7)
+    private String customerCode;    // 고객코드
+
+    @Column(length = 20)
+    private String customerEnglishName;    // 영문 고객명
+
+    @Column(length = 40)
+    private String itemList;
+}
