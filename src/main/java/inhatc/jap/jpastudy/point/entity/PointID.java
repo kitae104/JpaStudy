81ed8e3daa7cc5423d0da9795d0e259bb0084102
Kitae's Github <aqua0405@gmail.com>
2023-04-25 01:07:21 +0900

    Create PointID.java

---
+++ src/main/java/inhatc/jap/jpastudy/point/entity/PointID.java
@@ -0,0 +1,12 @@
+package inhatc.jap.jpastudy.point.entity;
+
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+public class PointID implements Serializable {
+
+    private String customerCode;    // 고객코드
+    private Long seqNo;
+
+}
