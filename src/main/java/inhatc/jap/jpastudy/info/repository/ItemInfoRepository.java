a8c05cfd78ba779b5310c75f5f03c99beef6baec
Kitae's Github <aqua0405@gmail.com>
2023-04-25 01:06:51 +0900

    Create ItemInfoRepository.java

---
+++ src/main/java/inhatc/jap/jpastudy/info/repository/ItemInfoRepository.java
@@ -0,0 +1,7 @@
+package inhatc.jap.jpastudy.info.repository;
+
+import inhatc.jap.jpastudy.info.entity.ItemInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemInfoRepository extends JpaRepository<ItemInfo, String> {
+}
