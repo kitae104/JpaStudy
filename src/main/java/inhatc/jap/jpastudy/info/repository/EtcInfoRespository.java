40030e2bcdad1a94b6e0e44453d90c7baf37280b
Kitae's Github <aqua0405@gmail.com>
2023-04-25 01:06:47 +0900

    Create EtcInfoRespository.java

---
+++ src/main/java/inhatc/jap/jpastudy/info/repository/EtcInfoRespository.java
@@ -0,0 +1,7 @@
+package inhatc.jap.jpastudy.info.repository;
+
+import inhatc.jap.jpastudy.info.entity.EtcInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EtcInfoRespository extends JpaRepository<EtcInfo, String> {
+}
