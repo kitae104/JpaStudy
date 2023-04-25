7887836483a28c175215b43b33367b7c53b79337
Kitae's Github <aqua0405@gmail.com>
2023-04-25 23:51:09 +0900

    Create TrainTimeRepositoryTest.java

---
+++ src/test/java/inhatc/jap/jpastudy/train/repository/TrainTimeRepositoryTest.java
@@ -0,0 +1,51 @@
+package inhatc.jap.jpastudy.train.repository;
+
+import inhatc.jap.jpastudy.sale.entity.Sales;
+import inhatc.jap.jpastudy.sale.repository.SalesRepository;
+import inhatc.jap.jpastudy.train.entity.TrainTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TrainTimeRepositoryTest {
+
+    @Autowired
+    private TrainTimeRepository trainTimeRepository;
+
+    @Test
+    @DisplayName("TrainTime 입력 테스트")
+    public void insertSalesTest(){
+        BufferedReader br = null;
+        String line = null;
+        try {
+            br = new BufferedReader(new FileReader(new File("src/main/resources/data/train_time.csv")));
+            while((line = br.readLine()) != null){
+                String[] datas = line.split(",");
+                Arrays.stream(datas).forEach(System.out::println);
+
+                TrainTime trainTime = TrainTime.builder()
+                        .trainNo(datas[0])
+                        .runOrder(Integer.parseInt(datas[1]))
+                        .stationName(datas[2])
+                        .arrivalTime(datas[3])
+                        .departureTime(datas[4].trim())
+                        .build();
+                trainTimeRepository.save(trainTime);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        trainTimeRepository.findAll().forEach(System.out::println);
+
+    }
+}
