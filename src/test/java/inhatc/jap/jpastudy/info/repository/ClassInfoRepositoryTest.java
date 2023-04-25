9e92423ae6d6c1de8ce07645dc0927c24f580dae
Kitae's Github <aqua0405@gmail.com>
2023-04-25 23:50:49 +0900

    Create ClassInfoRepositoryTest.java

---
+++ src/test/java/inhatc/jap/jpastudy/info/repository/ClassInfoRepositoryTest.java
@@ -0,0 +1,47 @@
+package inhatc.jap.jpastudy.info.repository;
+
+import inhatc.jap.jpastudy.info.entity.ClassInfo;
+import inhatc.jap.jpastudy.info.entity.ItemInfo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ClassInfoRepositoryTest {
+
+    @Autowired
+    private ClassInfoRepository classInfoRepository;
+
+    @Test
+    @DisplayName("ClassInfo 입력 테스트")
+    public void insertClassInfoTest(){
+        BufferedReader br = null;
+        String line = null;
+        try {
+            br = new BufferedReader(new FileReader(new File("src/main/resources/data/class_info.csv")));
+            while((line = br.readLine()) != null){
+                String[] datas = line.split(",");
+                Arrays.stream(datas).forEach(System.out::println);
+
+                ClassInfo classInfo = ClassInfo.builder()
+                        .classCode(datas[0])
+                        .className(datas[1])
+                        .build();
+                classInfoRepository.save(classInfo);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        classInfoRepository.findAll().forEach(System.out::println);
+
+    }
+}
