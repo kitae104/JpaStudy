77f2156712e21acc5d507062db6f127b18ea3822
Kitae's Github <aqua0405@gmail.com>
2023-05-03 01:37:04 +0900

    Update CustomerRepository.java

--- src/main/java/inhatc/jap/jpastudy/customer/repository/CustomerRepository.java
+++ src/main/java/inhatc/jap/jpastudy/customer/repository/CustomerRepository.java
@@ -3,5 +3,10 @@ package inhatc.jap.jpastudy.customer.repository;
 import inhatc.jap.jpastudy.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByCustomerName(String customerName);
+
+    List<Customer> findByFlag(String flag);
 }
